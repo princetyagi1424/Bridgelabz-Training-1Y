5d8301bb86033c8b745f6af18ef9bb2ffe12ebfd
princetyagi1424 <princetyagi3726@gmail.com>
2026-01-30 10:00:40 +0530

    Names

---
+++ array/Names.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Names{
+	public static void main(String [] args){
+		String [] names = {"Prince","Saksham","Harshit","Haris","Madhav","Lukesh",};
+		System.out.println("Names: ");
+		for(int i=0;i<=names.length;i++){
+			System.out.println(names[i]);
+		}
+	}
+}
