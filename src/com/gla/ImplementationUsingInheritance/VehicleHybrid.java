5bbe81d6d6f743c7c62c7cd8dbe7d41129788ab6
princetyagi1424 <princetyagi3726@gmail.com>
2026-03-18 22:55:05 +0530

    Added inheritance and polymorphism code

---
+++ src/com/gla/ImplementationUsingInheritance/VehicleHybrid.java
@@ -0,0 +1,6 @@
+package com.gla.ImplementationUsingInheritance;
+
+interface Refuelable{void refuel();}
+class PetrolVehicle implements Refuelable{public void refuel(){System.out.println("Refuel");}}
+class ElectricVehicle{void charge(){System.out.println("Charge");}}
+public class VehicleHybrid{public static void main(String[] args){PetrolVehicle p=new PetrolVehicle();p.refuel();}}
