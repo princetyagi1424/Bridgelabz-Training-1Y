4101b5be33366db98eb629ec264b26e0100d2a38
princetyagi1424 <princetyagi3726@gmail.com>
2026-04-19 13:19:29 +0530

    String API project

---
+++ src/com/gla/Methods/car.java
@@ -0,0 +1,15 @@
+package com.gla.Methods;
+import java.util.Scanner;
+public class car {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String name=sc.nextLine();
+        String company=sc.nextLine();
+        double price=sc.nextDouble();
+        String  model= sc.nextLine();
+
+
+
+    }
+}
